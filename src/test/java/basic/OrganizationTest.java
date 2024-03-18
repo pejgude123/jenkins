489c67c3a6b80405af4d226f724a99b78f37b55a
shree <shree@LAPTOP-R414LEBN>
2024-03-18 17:56:35 +0530

    added

--- src/test/java/basic/OrganizationTest.java
+++ src/test/java/basic/OrganizationTest.java
@@ -1,15 +1,45 @@
 package basic;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+
+
+
 
 public class OrganizationTest {
 	@Test
 	public void organizationTest()
 	{
-		WebDriver driver=new ChromeDriver();
+		
+		
+
+		
+			
+		WebDriver driver = new ChromeDriver();	
+		//To maximize the window
+		//wutil.maximize(driver);
+		//To apply wait for findelement()
+		//wutil.implicitwait(driver);
 		driver.get("https://locall.host/8888/");
+		
+		//To read data from Property File
+		//String URL = putil.getDataFromPropertyFile("Url");
+		//String USERNAME =putil.getDataFromPropertyFile("Username");
+		//String PASSWORD = putil.getDataFromPropertyFile("Password");
+		
+		
+		//To launch the application
+	
+		
+		
+		// Login to application 
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
 		
 	}
 	
